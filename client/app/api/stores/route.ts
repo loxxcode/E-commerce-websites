6e952391db4d38457cffdf554a75e114331acb95
loxxcode <loxxhypolite@gmail.com>
2026-02-25 13:07:44 +0200

    fix errors have

--- client/app/api/stores/route.ts
+++ client/app/api/stores/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
-const filePath = path.join(process.cwd(), "server", "stores.json");
+const filePath = path.join(process.cwd(), "..", "server", "stores.json");
 
 function ensureFile() {
   const dir = path.dirname(filePath);
